a653b088d8f6bed9e21917be84267140d113fc43
Neptune Magic Sauce <not.an.email@email.email>
2025-01-29 19:50:45 +0100

    moving functions to dedicated files

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,18 @@
 import subprocess
 import sys
 import os
+import time
+import random
+
+k_data_dir = "data"
+
+def init(app_name):
+    random.seed(time.time())
+
+    data_path = GetDataPath(app_name) + "/" + k_data_dir
+    if not os.path.isdir(data_path):
+        os.mkdir(data_path)
+    os.chdir(data_path)
 
 def GetWindowsEnvVar(var_name):
     return subprocess.run(["cmd.exe", "/c", "echo", "%" + var_name + "%"], capture_output=True).stdout.strip().decode("utf-8")
@@ -40,3 +52,27 @@ def GetDataPath(app_name):
     else:
         os.mkdir(path)
     return path
+
+class ProgressBar:
+    def __init__(self, count, prefix='', size=60, out=sys.stdout):
+        self.count = count
+        self.prefix = prefix + ' '
+        self.size = size
+        self.out = out
+        self.start = time.time()
+        self.tick(0)
+    def tick(self, index):
+        if self.count <= 0:
+            return
+        j = min(self.count, index + 1)
+        x = int(self.size*j/self.count)
+        current = time.time()
+        if j <= 0:
+            remaining = 0
+        else:
+            remaining = ((current - self.start) / j) * (self.count - j)
+        mins, sec = divmod(remaining, 60)
+        time_str = f"{int(mins):02}m:{int(sec):02}s"
+        print(f"{self.prefix}[{u'#'*x}{('.'*(self.size-x))}] {index}/{self.count} ETA {time_str}", end='\r', file=self.out, flush=True)
+        if j == self.count:
+            print('')
