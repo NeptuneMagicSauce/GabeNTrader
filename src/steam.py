258004e38d30282af2451b65ae2a8be0411934fd
Neptune Magic Sauce <37757244+NeptuneMagicSauce@users.noreply.github.com>
2025-01-30 22:18:17 +0100

    cache user id

--- src/steam.py
+++ src/steam.py
@@ -1,12 +1,22 @@
 import re
 
 from network import *
+from utils import *
+
+k_userid_path = "userid"
 
 def GetUserId():
+    try:
+        return pickle_load(k_userid_path)
+    except:
+        pass
+
     c = fetcher.get_text("https://steamcommunity.com/my/profile")
     if c is None:
         return None
+
     regexp = re.compile('g_steamID\\s=\\s"(.*)"')
+
     for l in c.splitlines():
         match = regexp.search(l)
         if match:
@@ -14,13 +24,18 @@ def GetUserId():
             if len(groups):
                 id = groups[0]
                 try:
-                    return int(id)
+                    ret = int(id)
                 except:
                     print('failed to convert user id to string', l)
+                    return None
+                try:
+                    pickle_save(ret, k_userid_path, compress=False)
+                except:
+                    pass
+                return ret
             else:
                 print('user id: regexp matched but no groups')
     return None
 
-
 # TODO run in parallel as soon as possible
-# TODO cache result of UserID (per cookie?)
+# TODO invalidate cache: if cookie changed or if change account
