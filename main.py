e8177073188817ee40735c7bdfb54da29626c2b7
Neptune Magic Sauce <not.an.email@email.email>
2025-02-04 21:33:36 +0100

    refactoring the 'data' function

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-import requests
-import json
-import pickle
 import os
 import sys
 
@@ -14,6 +11,7 @@ from cookie import *
 from utils import *
 from network import *
 from steam import *
+from data import *
 
 # import pandas as pd
 # import numpy as np
@@ -21,97 +19,12 @@ from steam import *
 # import matplotlib.pyplot as plt
 # import seaborn as sns
 
-k_app_name = "GabeNTrader"
-k_game_id = "730"
-
-def get_items():
-
-    items = []
-    index = 0
-
-    cache_path = "items.pkl"
-    try:
-        items = pickle_load(cache_path)
-        index = len(items)
-        print("InCache:", index)
-    except:
-        print("Failed to load cache from " + cache_path)
-
-    item_count_json = Instances.fetcher.get_json("https://steamcommunity.com/market/search/render/?search_descriptions=0&sort_column=default&sort_dir=desc&appid="+k_game_id+"&norender=1&count=1")
-
-    if item_count_json is None:
-        exit(1)
-
-    if not 'total_count' in item_count_json:
-        print('total_count not found')
-        return
-
-    item_count = item_count_json['total_count'] # get total count
-    # item_count = min(item_count, 14000)
-    # item_count = 500
-
-    print("MarketItems:", item_count)
-
-    k_item_per_req = 100
-    # k_item_per_req = 2
-
-    progress = ProgressBar(item_count - index)
-    fetched = 0
-
-    while index < item_count:
-
-        items_json = Instances.fetcher.get_json("https://steamcommunity.com/market/search/render/?start=" + str(index) + "&search_descriptions=0&sort_column=default&sort_dir=desc&appid=" + k_game_id + "&norender=1&count=" + str(k_item_per_req))
-
-        if items_json is None:
-            print("json parsing returned None")
-            break
-
-        if not 'results' in items_json:
-            print("'results' not found:")
-            print(json.dumps(allItems, indent=1))
-            break
-
-        items_json = items_json['results'];
-
-        count = len(items_json)
-
-        if count == 0:
-            print("results length = 0")
-            break
-
-        items.extend(items_json)
-
-        fetched += count
-        index += count
-        progress.tick(fetched)
-
-    # TODO only save to cache if recomputed
-    pickle_save(items, cache_path)
-
-    # extract hash_name
-    names = set()
-    for i in items:
-        names.add(i['hash_name'])
-    pickle_save(names, "names.pkl")
-
-    print("Loaded:", len(items))
-    print("Names:", len(names))
-
-# def get_inventory():
-#     print("fetch my inventory")
-#     # request = requests.get("https://steamcommunity.com/market/&norender=1", cookies=cookie)
-#     request = requests.get("https://steamcommunity.com/my/inventory/&norender=1")#, cookies=cookie)
-#     print("returned", request.status_code)
-#     print("request", request)
-#     print("request.content", request.content)
-#     as_json = json.loads(request.content)
-#     json.dumps(as_json, indent=1)
 
 ############
 ### MAIN ###
 ############
 
-Utils.initialize(k_app_name)
+Utils.initialize("GabeNTrader")
 
 Cookie.initialize()
 
@@ -128,11 +41,3 @@ if not Instances.cookie_is_valid:
 Steam.get_user_name()
 
 get_items()
-
-# TODO cookie: ask to login in webview and use its cookie?
-# TODO cookie: use the one from chrome?
-
-# TODO invalidate cached items if total_count changed
-# needs another serialized bit: is pickle finished?
-# because cache size can not be compared without that
-# TODO list inventory
