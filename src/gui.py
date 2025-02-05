f7616484ba62e6e57595923b972dbc15b8501cbe
Neptune Magic Sauce <not.an.email@email.email>
2025-02-05 22:50:33 +0100

    integrate webview with qt framework, both want to be on main thread

--- src/gui.py
+++ src/gui.py
@@ -1,8 +1,14 @@
+# https://www.riverbankcomputing.com/static/Docs/PyQt6/index.html
+# https://doc.qt.io/qt-6/qtwidgets-module.html
+
 import sys
+import os
 import time
 import threading
+import webview
 
 from PyQt6.QtCore import QDateTime, Qt, QTimer
+from PyQt6.QtCore import QObject, pyqtSignal
 from PyQt6.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
         QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
         QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
@@ -14,11 +20,30 @@ from utils import *
 
 
 class GUI:
-    return_code = None
-    def initialize():
-        print('GUI >>>', threading.current_thread())
+    app = None
 
-        app = QApplication(sys.argv)
+    def wait_for_load():
+        assert(threading.current_thread() is not threading.main_thread())
+        while GUI.app is None:
+            time.sleep(0.01)
+
+    class App(QApplication):
+        start_webview = pyqtSignal()
+        webview_finished = threading.Event()
+
+        def __init__(self):
+            super().__init__(sys.argv)
+            self.start_webview.connect(self.start_webview_cb)
+
+        def start_webview_cb(self):
+            k_webview_storage = os.getcwd() + '/webview/' + OScompat.id_str
+            webview.start(private_mode=False, storage_path=k_webview_storage)
+            self.webview_finished.set()
+
+    def run():
+        print('GUI >>>', threading.current_thread())
+        GUI.app = GUI.App()
+        # instantiate widgets must be after ctor QApplication
         b = QLineEdit()
         b.show()
-        return_code = app.exec()
+        return GUI.app.exec()
