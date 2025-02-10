2448f1786c19f44d86566f48f85d72a590176455
Neptune Magic Sauce <not.an.email@email.email>
2025-02-10 22:43:04 +0100

    cmdline utility get.py: do not print to gui but to console

--- get.py
+++ get.py
@@ -18,7 +18,7 @@ Utils.initialize('GabeNTrader')
 Cookie.initialize()
 
 if len(sys.argv) != 2:
-    print("Usage:", sys.argv[0], "url_to_fetch")
+    builtins.print("Usage:", sys.argv[0], "url_to_fetch")
     exit(1)
 url = sys.argv[1]
 
@@ -28,14 +28,14 @@ if not Fetcher.check(req):
     exit(1)
 
 if len(req.content) <= 0:
-    print("content length zero")
+    builtins.print("content length zero")
     exit(1)
 
 try:
     as_json = json.loads(req.content)
 except Exception as e:
-    print("did not parse JSON", file=sys.stderr)
-    print(req.text)
+    builtins.print("did not parse JSON", file=sys.stderr)
+    builtins.print(req.text)
     exit(1)
 
 as_string = json.dumps(as_json, indent=1)
@@ -43,7 +43,7 @@ as_string = json.dumps(as_json, indent=1)
 if sys.stdout.isatty() and shutil.which("batcat"):
     with tempfile.NamedTemporaryFile(delete_on_close=False) as tmp:
         tmp.close()
-        print(as_string, file=open(tmp.name, 'w'))
+        builtins.print(as_string, file=open(tmp.name, 'w'))
         subprocess.run([ "batcat", "-l", "json", "--theme", "Monokai Extended", "--file-name", url, tmp.name])
 else:
-    print(as_string)
+    builtins.print(as_string)
