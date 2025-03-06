fe4779784d514df0461dc4a8350f9670ea27f724
Neptune Magic Sauce <not.an.email@email.email>
2025-03-06 12:37:57 +0100

    clean up commented code

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,7 @@
-#include <iostream>
-#include <QTimer>
-
 #include "gui.hpp"
-
-// #include "bar.hpp"
-// import foo;
 
 int main(int /* argc*/, char **/* argv*/)
 {
-  // Foo{}.foo();
   auto g = GUI{};
-
-
-  // QTimer::singleShot((int)2000, [] () {
-  //   std::cout << "main() qtimer fired" << std::endl;
-  // } );
-
-
   return g.exec();
 }
